b0fb39142db6207d5a6259921fed1854181e9999
alex <tte577es7@yandex.ru>
2020-07-14 13:10:54 +0300

    Продолжаю смотреть урок по галпу и учиться

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # AssemblyGULP
+Видео урок:
+https://www.youtube.com/watch?v=stFOy0Noahg&t=1169s
+timecode: 42:52
 
+Установленные дополнения:
  npm cache clean --force
- gulp
  npm i npm -g
+ npm i browser-sync --save-dev
+ npm i gulp-file-include --save-dev
+ npm i del --save-dev
+ npm i gulp-sass --save-dev
+ npm i gulp-autoprefixer --save-dev
--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ let path = {
 		fonts: project_folder + "/fonts/",
 	},
 	src:{
-		html: source_folder + "/",
+		html: [source_folder + "/*.html" , "!" + source_folder + "/_*.html"],
 		css: source_folder + "/scss/style.scss",
 		js: source_folder + "/js/script.js",
 		img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
@@ -32,3 +32,65 @@ let path = {
 
 
 let {src, dest} = require('gulp'),
+	gulp = require('gulp'),
+	browsersync = require('browser-sync').create(),
+	fileinclude = require('gulp-file-include'),
+	del = require('del'),
+	scss = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer');
+
+function browserSync(params) {
+	browsersync.init({
+		server:{
+			baseDir: "./" + project_folder + "/"
+		},
+		port: 3000,
+		notify: false
+	})
+}
+
+function html(){
+	return src(path.src.html)
+		.pipe(fileinclude())
+		.pipe(dest(path.build.html))
+		.pipe(browsersync.stream())
+}
+
+function css(){
+	return src(path.src.css)
+		.pipe(
+			scss({
+				outputStyle: "expanded"
+			})
+		)
+		.pipe(
+			autoprefixer({
+				overrideBrowserlist: ["last 5 version"]
+			})
+		)
+		.pipe(dest(path.build.css))
+		.pipe(browsersync.stream())
+}
+
+function watchFiles(){
+	gulp.watch([path.watch.html] , html);
+	gulp.watch([path.watch.css] , css);
+}
+
+function clean(params){
+	return del(path.clean);
+}
+
+let build = gulp.series( clean , gulp.parallel(css,html) );
+let watch = gulp.parallel( build , watchFiles , browserSync );
+
+
+exports.build = build;
+exports.css = css;
+exports.html = html;
+exports.watch = watch;
+exports.default = watch;
+
+
+
+
