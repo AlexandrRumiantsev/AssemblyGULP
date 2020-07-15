42805f29cd2ea653cdbf152d16fadc00298fff50
alex <tte577es7@yandex.ru>
2020-07-15 16:09:47 +0300

    Последний шаг

--- README.md
+++ README.md
@@ -1,13 +1,43 @@
 # AssemblyGULP
 Видео урок:
 https://www.youtube.com/watch?v=stFOy0Noahg&t=1169s
-timecode: 42:52
 
-Установленные дополнения:
- npm cache clean --force
- npm i npm -g
- npm i browser-sync --save-dev
- npm i gulp-file-include --save-dev
- npm i del --save-dev
- npm i gulp-sass --save-dev
- npm i gulp-autoprefixer --save-dev
+Инструкции для запуска:
+gulp  - общая сборка
+gulp svgSprite - отдельный таск для svg файлов
+
+Установленные плагины:
+| Название       | Команда               | Описание |
+| ------------- |:------------------:| -----:|
+| browser-sync | npm i browser-sync --save-dev | позволяет тестировать ваше приложение сразу на всех браузерах одновременно. В этом уроке мы разберем, как менее чем за 5 минут, к вашему проекту можно подключить BrowserSync в режиме livereload и настроить синхронизацию между всеми браузерами. | 
+| gulp-file-include | npm i gulp-file-include --save-dev | Подключение файлов в файле | 
+| del | npm i del --save-dev | Удаление в галпе | 
+| gulp-sass | npm i gulp-sass --save-dev | работа в файлами saas | 
+| gulp-autoprefixer | npm i gulp-autoprefixer --save-dev |  Autoprefixer сканирует ваши CSS файлы, и автоматически проставляет префиксы к css свойствам. | 
+| gulp-group-css-media-queries | npm i gulp-group-css-media-queries --save-dev | Группировка css-селекторов Gulp | 
+| gulp-clean-css | npm i gulp-clean-css --save-dev | Минификация CSS | 
+| gulp-rename | npm i --save-dev gulp-rename | Переименование файлов в галпе | 
+| gulp-uglify-es | npm i --save-dev gulp-uglify-es |  Минификация JS | 
+| gulp-imagemin | npm i --save-dev gulp-imagemin |  Минификация Изображений | 
+| gulp-webp | npm i --save-dev gulp-webp |  Оптимизация изображений с использованием формата WebP | 
+| gulp-webp-html | npm i --save-dev gulp-webp-html | Оптимизация html с использованием формата WebP | 
+| gulp-webp-css | npm i --save-dev gulp-webp-css |  Оптимизация css с использованием формата WebP | 
+| gulp-svg-sprite | npm i --save-dev gulp-svg-sprite | Оптимизация svg спрайтов | 
+| gulp-ttf2woff, gulp-ttf2woff2 | npm i --save-dev gulp-ttf2woff gulp-ttf2woff2 |  Оптимизация шрифтов | 
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,19 @@ let {src, dest} = require('gulp'),
 	fileinclude = require('gulp-file-include'),
 	del = require('del'),
 	scss = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer');
+	autoprefixer = require('gulp-autoprefixer'),
+	group_media = require('gulp-group-css-media-queries'),
+    clean_css = require('gulp-clean-css'),
+    remane = require('gulp-rename'),
+    uglify = require('gulp-uglify-es').default,
+    imagemin = require('gulp-imagemin'),
+    webp = require('gulp-webp'),
+    webphtml = require('gulp-webp-html'),
+    webpcss = require('gulp-webp-css'),
+    svgSprite = require('gulp-svg-sprite'),
+    ttf2woff = require('gulp-ttf2woff'),
+    ttf2woff2 = require('gulp-ttf2woff2');
+
 
 function browserSync(params) {
 	browsersync.init({
@@ -52,8 +64,39 @@ function browserSync(params) {
 function html(){
 	return src(path.src.html)
 		.pipe(fileinclude())
+		.pipe(webphtml())
 		.pipe(dest(path.build.html))
 		.pipe(browsersync.stream())
+}
+
+function images(){
+	return src(path.src.img)
+	    .pipe(
+	    	webp({
+	    		quality: 70
+	    	})
+	    )
+	    .pipe(dest(path.build.img))
+	    .pipe(src(path.src.img))
+		.pipe(
+			imagemin({
+				progressive: true,
+				svgoPlugin: [{ removeViewBox: false }],
+				interlaced: true,
+				optimizationLevel: 3
+			})
+		)
+		.pipe(dest(path.build.img))
+		.pipe(browsersync.stream())
+}
+
+function fonts(){
+	src(path.src.fonts)
+	.pipe(ttf2woff())
+	.pipe(dest(path.build.fonts))
+	return src(path.src.fonts)
+	.pipe(ttf2woff2())
+	.pipe(dest(path.build.fonts))
 }
 
 function css(){
@@ -64,27 +107,78 @@ function css(){
 			})
 		)
 		.pipe(
+			group_media()
+		)
+		.pipe(
 			autoprefixer({
-				overrideBrowserlist: ["last 5 version"]
+				overrideBrowserlist: ["last 5 version"],
+				cascade: true
+			})
+		)
+		.pipe(
+			webpcss()
+		)
+		.pipe(dest(path.build.css))
+		.pipe(
+			clean_css()
+		)
+		.pipe(
+			/* Минификация scss файла */
+			remane({
+				extname: ".min.css"
 			})
 		)
 		.pipe(dest(path.build.css))
 		.pipe(browsersync.stream())
 }
 
+function js(){
+	return src(path.src.js)
+		.pipe(fileinclude())
+		.pipe(dest(path.build.js))
+		.pipe(
+			uglify()
+		)
+		.pipe(
+			remane({
+				extname: ".min.js"
+			})
+		)
+		.pipe(dest(path.build.js))
+		.pipe(browsersync.stream())
+}
+
 function watchFiles(){
 	gulp.watch([path.watch.html] , html);
 	gulp.watch([path.watch.css] , css);
+	gulp.watch([path.watch.js] , js);
+	gulp.watch([path.watch.img] , images);
 }
+
+gulp.task('svgSprite' , function(){
+	return gulp.src([source_folder + "/iconsprite/*.svg"])
+		.pipe(svgSprite({
+			mode: {
+				stack:{
+					sprite: "../icons/icons.svg" ,
+					example: true
+				}
+			}
+		})
+	    )
+	    .pipe(dest(path.build.img))
+	}) 
 
 function clean(params){
 	return del(path.clean);
 }
 
-let build = gulp.series( clean , gulp.parallel(css,html) );
+let build = gulp.series( clean , gulp.parallel(js,css,html,images,fonts) );
 let watch = gulp.parallel( build , watchFiles , browserSync );
 
-
+exports.fonts = fonts;
+exports.images = images;
+exports.js = js;
 exports.build = build;
 exports.css = css;
 exports.html = html;
